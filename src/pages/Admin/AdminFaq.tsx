98630107605d22727eb63c1b2b6ebc9e07eb6195
Tusho7 <sandrotushurashvili7@gmail.com>
2024-09-06 11:39:54 +0400

    style: update textares style in admin faq page

--- src/pages/Admin/AdminFaq.tsx
+++ src/pages/Admin/AdminFaq.tsx
@@ -216,7 +216,7 @@ const Faq = () => {
                         <textarea
                           value={editAnswer}
                           onChange={handleAnswerChange}
-                          className="w-full mt-2 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                          className="w-full mt-2 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm resize-none"
                         />
                         <button
                           onClick={handleSaveClick}
