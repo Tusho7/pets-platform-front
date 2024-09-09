3b58346bd43be744ecc0b5ea13882140609b9563
Tusho7 <sandrotushurashvili7@gmail.com>
2024-09-09 12:46:07 +0400

    style: update styles

--- src/pages/Admin/AdminUsers.tsx
+++ src/pages/Admin/AdminUsers.tsx
@@ -114,122 +114,123 @@ const AdminUsers = () => {
   });
 
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100 ">
       <Header />
-      <div className="p-6 relative">
-        <h1 className="text-3xl font-bold text-gray-800 mb-6 text-center">
-          {t("adminUsers.title")}
-        </h1>
-        <div className="mb-4">
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={handleSearchChange}
-            placeholder={t("adminUsers.searchPlaceholder")}
-            className="p-2 border border-gray-300 rounded w-full"
-          />
-        </div>
-        {filteredUsers.length === 0 ? (
-          <p className="text-center text-gray-600">{t("adminUsers.noUsers")}</p>
-        ) : (
-          <div className="overflow-x-auto">
-            <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-lg relative">
-              <thead>
-                <tr className="bg-gray-100 text-gray-600">
-                  <th className="px-6 py-3 text-left text-sm font-medium">
-                    {t("adminUsers.profile")}
-                  </th>
-                  <th className="px-6 py-3 text-left text-sm font-medium">
-                    {t("adminUsers.name")}
-                  </th>
-                  <th className="px-6 py-3 text-left text-sm font-medium">
-                    {t("adminUsers.email")}
-                  </th>
-                  <th className="px-6 py-3 text-left text-sm font-medium">
-                    {t("adminUsers.phone")}
-                  </th>
-                  <th className="px-6 py-3 text-left text-sm font-medium">
-                    {t("adminUsers.status")}
-                  </th>
-                  <th className="px-6 py-3 text-left text-sm font-medium">
-                    {t("adminUsers.userId")}
-                  </th>
-                  <th className="px-6 py-3 text-left text-sm font-medium">
-                    {t("adminUsers.actions")}
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="text-gray-600">
-                {filteredUsers.map((user: User) => (
-                  <tr
-                    key={user.id}
-                    className="border-b border-gray-200 hover:bg-gray-50 relative"
-                  >
-                    <td className="px-6 py-4">
-                      <img
-                        src={
-                          import.meta.env.VITE_API_STORAGE + user.profilePicture
-                        }
-                        alt="Profile"
-                        className="w-10 h-10 rounded-full object-cover"
-                      />
-                    </td>
-                    <td className="px-6 py-4">
-                      {user.firstName} {user.lastName}
-                    </td>
-                    <td className="px-6 py-4">{user.email}</td>
-                    <td className="px-6 py-4">{user.phoneNumber || "N/A"}</td>
-                    <td className="px-6 py-4">
-                      {user.isBlocked ? (
-                        <span className="flex items-center text-red-600">
-                          <FaBan className="mr-2" /> {t("adminUsers.blocked")}
-                        </span>
-                      ) : (
-                        <span className="text-green-600">
-                          {t("adminUsers.active")}
-                        </span>
-                      )}
-                    </td>
-                    <td className="px-6 py-4">{user.id}</td>
-                    <td className="px-6 py-4 relative">
-                      <button
-                        onClick={(event) => handleDropdownToggle(event, user)}
-                        className="text-gray-600 hover:text-gray-800 focus:outline-none"
-                      >
-                        <FaEllipsisV className="text-xl" />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            {dropdownVisible && selectedUser && (
-              <div
-                style={{
-                  top: dropdownPosition?.top,
-                  left: dropdownPosition?.left,
-                }}
-                className="fixed bg-white border border-gray-200 rounded-lg shadow-lg z-50"
-              >
-                <button
-                  onClick={() => handleOptionClick(selectedUser, "block")}
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
-                >
-                  {selectedUser.isBlocked
-                    ? t("adminUsers.unblock")
-                    : t("adminUsers.block")}
-                </button>
-                <button
-                  onClick={() => handleOptionClick(selectedUser, "delete")}
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
-                >
-                  {t("adminUsers.delete")}
-                </button>
-              </div>
-            )}
+      <section className="p-4">
+        <div className="max-w-[1200px] mx-auto">
+          <h1 className="text-4xl font-extrabold text-gray-800 mb-6 text-center">
+            {t("adminUsers.title")}
+          </h1>
+          <div className="mb-6">
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              placeholder={t("adminUsers.searchPlaceholder")}
+              className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
           </div>
-        )}
-      </div>
+          {filteredUsers.length === 0 ? (
+            <p className="text-center text-gray-600">
+              {t("adminUsers.noUsers")}
+            </p>
+          ) : (
+            <div className="overflow-x-auto bg-white shadow-md rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th className="px-6 py-3 text-left text-sm font-semibold text-gray-600">
+                      {t("adminUsers.profile")}
+                    </th>
+                    <th className="px-6 py-3 text-left text-sm font-semibold text-gray-600">
+                      {t("adminUsers.name")}
+                    </th>
+                    <th className="px-6 py-3 text-left text-sm font-semibold text-gray-600">
+                      {t("adminUsers.email")}
+                    </th>
+                    <th className="px-6 py-3 text-left text-sm font-semibold text-gray-600">
+                      {t("adminUsers.phone")}
+                    </th>
+                    <th className="px-6 py-3 text-left text-sm font-semibold text-gray-600">
+                      {t("adminUsers.status")}
+                    </th>
+                    <th className="px-6 py-3 text-left text-sm font-semibold text-gray-600">
+                      {t("adminUsers.userId")}
+                    </th>
+                    <th className="px-6 py-3 text-left text-sm font-semibold text-gray-600">
+                      {t("adminUsers.actions")}
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200">
+                  {filteredUsers.map((user: User) => (
+                    <tr key={user.id} className="hover:bg-gray-50">
+                      <td className="px-6 py-4">
+                        <img
+                          src={`${import.meta.env.VITE_API_STORAGE}${
+                            user.profilePicture
+                          }`}
+                          alt="Profile"
+                          className="w-12 h-12 rounded-full object-cover"
+                        />
+                      </td>
+                      <td className="px-6 py-4">
+                        {user.firstName} {user.lastName}
+                      </td>
+                      <td className="px-6 py-4">{user.email}</td>
+                      <td className="px-6 py-4">{user.phoneNumber || "N/A"}</td>
+                      <td className="px-6 py-4">
+                        {user.isBlocked ? (
+                          <span className="flex items-center text-red-600">
+                            <FaBan className="mr-2" /> {t("adminUsers.blocked")}
+                          </span>
+                        ) : (
+                          <span className="text-green-600">
+                            {t("adminUsers.active")}
+                          </span>
+                        )}
+                      </td>
+                      <td className="px-6 py-4">{user.id}</td>
+                      <td className="px-6 py-4 text-right">
+                        <button
+                          onClick={(event) => handleDropdownToggle(event, user)}
+                          className="text-gray-600 hover:text-gray-800 focus:outline-none"
+                        >
+                          <FaEllipsisV className="text-xl" />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              {dropdownVisible && selectedUser && (
+                <div
+                  style={{
+                    top: dropdownPosition?.top,
+                    left: dropdownPosition?.left,
+                  }}
+                  className="fixed bg-white border border-gray-300 rounded-lg shadow-lg z-50"
+                >
+                  <button
+                    onClick={() => handleOptionClick(selectedUser, "block")}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                  >
+                    {selectedUser.isBlocked
+                      ? t("adminUsers.unblock")
+                      : t("adminUsers.block")}
+                  </button>
+                  <button
+                    onClick={() => handleOptionClick(selectedUser, "delete")}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+                  >
+                    {t("adminUsers.delete")}
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </section>
     </div>
   );
 };
