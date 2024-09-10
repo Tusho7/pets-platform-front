f460b0edd575a519a497cc16b02fea832b4cfbfc
Tusho7 <sandrotushurashvili7@gmail.com>
2024-09-10 10:14:10 +0400

    style: add background image for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import Registration from "../modals/Registration";
 import { useTranslation } from "react-i18next";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import PetBackground from "/pet_background5.jpg";
 
 const Home = () => {
   const { t } = useTranslation();
@@ -26,28 +27,36 @@ const Home = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center text-black relative ">
+    <div className="min-h-screen flex flex-col items-center justify-center text-wh font-extrabold relative  ">
       <Header openLoginModal={openLoginModal} />
-      <main className="flex-grow flex flex-col items-center justify-center text-center p-6">
-        <h2 className="text-3xl md:text-4xl font-semibold mb-4 animate-fade-in">
-          {t("home.welcome")}
-        </h2>
-        <p className="text-lg md:text-xl max-w-md mx-auto mb-6">
-          {t("home.description")}
-        </p>
-        <div className="flex gap-4 mt-6">
-          <button
-            className="bg-blue-500 hover:bg-blue-600 text-white py-3 px-6 rounded-full shadow-lg transition-all duration-300"
-            onClick={() => {}}
-          >
-            {t("home.lostPets")}
-          </button>
-          <button
-            className="bg-gray-500 hover:bg-gray-600 text-white py-3 px-6 rounded-full shadow-lg transition-all duration-300"
-            onClick={() => {}}
-          >
-            {t("home.streetPets")}
-          </button>
+      <main
+        className="flex-grow flex flex-col items-center justify-center text-center p-6 bg-no-repeat w-full bg-center bg-cover "
+        style={{
+          backgroundImage: `url(${PetBackground})`,
+          backgroundPosition: `center -190px`,
+        }}
+      >
+        <div className="flex flex-col justify-center items-center -mt-40">
+          <h2 className="text-3xl md:text-4xl font-semibold mb-4 animate-fade-in">
+            {t("home.welcome")}
+          </h2>
+          <p className="text-lg md:text-xl max-w-md mx-auto mb-6">
+            {t("home.description")}
+          </p>
+          <div className="flex gap-4 mt-6">
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white py-3 px-6 rounded-full shadow-lg transition-all duration-300"
+              onClick={() => {}}
+            >
+              {t("home.lostPets")}
+            </button>
+            <button
+              className="bg-gray-500 hover:bg-gray-600 text-white py-3 px-6 rounded-full shadow-lg transition-all duration-300"
+              onClick={() => {}}
+            >
+              {t("home.streetPets")}
+            </button>
+          </div>
         </div>
       </main>
 
