551952c7a1c989f860c69285a06c4c5c16ce965f
Tusho7 <sandrotushurashvili7@gmail.com>
2024-09-19 11:49:47 +0400

    chore: update translations for profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -90,7 +90,7 @@ const Profile = () => {
       <Header />
       <main className="p-4 min-h-screen  ">
         <h1 className="text-2xl font-bold mb-4 max-w-[1200px] mx-auto">
-          {t("profile.viewProfile")}
+          {t("profile.profile")}
         </h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-[1200px] mx-auto">
